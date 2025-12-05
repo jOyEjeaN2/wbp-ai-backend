ea842abe9ad2a1d08739ddd6d8495bd77c05cf30
jOyEjiN2 <joyejin@joyejin-ui-MacBookPro.local>
2025-12-05 15:42:08 +0900

    외부 컴퓨터 접속을 위해 CORS 및 host 설정 수정

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ app = FastAPI(
 )
 
 origins = [
+    "http://172.20.4.84:5500",
+    "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "http://127.0.0.1:8000/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
